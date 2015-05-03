9fd714083558c757fbe0606a41dbca43c73edc7d
Jack Kingsman <jack.kingsman@gmail.com>
2015-05-02 20:35:40 -0700

    formatted and linted

--- src/routes_twilio.js
+++ src/routes_twilio.js
@@ -2,38 +2,26 @@
 
 router.get('/twilio/sms/', function(req, res) {
   var message,
-  query = "INSERT INTO whistles VALUES(?, ?, ?)",
-  param;
-  
-  if(req.query.NumMedia > 0){
+    query = "INSERT INTO whistles VALUES(?, ?, ?)",
+    param;
+
+  if (req.query.NumMedia > 0) {
     //this message has media; disregard the text
-  }
-  else{
+  } else {
     //this query is just a text
-  var param = [Date.now(), 0, req.query.Body];
-    
-  //run the query
-  db.run(query, param, function(err) {
-    if (err) {
-      res.status(500).json({
-        error: err
-      });
-    } else {
-      //send back TWIML response
-      res.type('text/html'); // set content-type
-      res.send('<?xml version="1.0" encoding="UTF-8"?><Response><Sms>Thank you; your whistle has been recieved.</Sms></Response>');
-    }
-  });
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    param = [Date.now(), 0, req.query.Body];
+
+    //run the query
+    db.run(query, param, function(err) {
+      if (err) {
+        res.status(500).json({
+          error: err
+        });
+      } else {
+        //send back TWIML response
+        res.type('text/html'); // set content-type
+        res.send('<?xml version="1.0" encoding="UTF-8"?><Response><Sms>Thank you; your whistle has been recieved.</Sms></Response>');
+      }
+    });
   }
 });
