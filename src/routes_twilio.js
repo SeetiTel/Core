c0015cf54dd56c3e40f6dcc042c3b4a3c97951d5
Jack Kingsman <jack.kingsman@gmail.com>
2015-05-02 21:07:25 -0700

    adding debugs

--- src/routes_twilio.js
+++ src/routes_twilio.js
@@ -8,29 +8,36 @@ router.get('/twilio/sms/', function(req, res) {
 
   if (req.query.NumMedia > 0) {
     //this message has media; disregard the text
-    
+
     //create a stream to put the file in, request it, and store it.
     //the URL where it's kept is in req.query.MediaUrl0
     var file = fs.createWriteStream("data/" + filename);
-var request = https.get(req.query.MediaUrl0, function(response) {
-  response.pipe(file);
-  
-  param = [Date.now(), 2, "data\\" + filename];
+    var request = https.get(req.query.MediaUrl0, function(response) {
+      //save the file
+      response.pipe(file);
 
-    //run the query
-    db.run(query, param, function(err) {
-      if (err) {
-        //send back TWIML response
-        res.type('text/html'); // set content-type
-        res.send('<?xml version="1.0" encoding="UTF-8"?><Response><Sms>Sorry, something went wrong. Please try again.</Sms></Response>');
-        console.log(chalk.red("Error! " + err));
-      } else {
-        //send back TWIML response
-        res.type('text/html'); // set content-type
-        res.send('<?xml version="1.0" encoding="UTF-8"?><Response><Sms>Thank you; your whistle has been recieved.</Sms></Response>');
-      }
+      //DEBUG
+      console.log(req.query.MediaUrl0);
+      console.log(req.query);
+
+
+      //set up our query
+      param = [Date.now(), 2, "data\\" + filename];
+
+      //run the query
+      db.run(query, param, function(err) {
+        if (err) {
+          //send back TWIML response
+          res.type('text/html'); // set content-type
+          res.send('<?xml version="1.0" encoding="UTF-8"?><Response><Sms>Sorry, something went wrong. Please try again.</Sms></Response>');
+          console.log(chalk.red("Error! " + err));
+        } else {
+          //send back TWIML response
+          res.type('text/html'); // set content-type
+          res.send('<?xml version="1.0" encoding="UTF-8"?><Response><Sms>Thank you; your whistle has been recieved.</Sms></Response>');
+        }
+      });
     });
-});
   } else {
     //this query is just a text
     param = [Date.now(), 0, req.query.Body];
